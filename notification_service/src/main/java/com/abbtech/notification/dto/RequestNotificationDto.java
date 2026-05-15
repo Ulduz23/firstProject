369462406f4336a1d5bfc3073612f0cde49628ae
Ulduz Quliyeva <qlyvaulduz@gmail.com>
2026-05-15 17:35:45 +0400

    notificatrion swagger

--- notification_service/src/main/java/com/abbtech/notification/dto/RequestNotificationDto.java
+++ notification_service/src/main/java/com/abbtech/notification/dto/RequestNotificationDto.java
@@ -1,18 +1,29 @@
 package com.abbtech.notification.dto;
 
 import com.abbtech.notification.model.enums.NotificationStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
+@Schema(example = """
+        {
+          "userId": 1,
+          "message": "Order payment completed successfully",
+          "status": "NEW"
+        }
+        """)
 public record RequestNotificationDto(
         @NotNull(message = "User id bos ola bilmez")
+        @Schema(example = "1")
         Long userId,
         @NotBlank(message = "Message bos ola bilmez")
         @Size(max = 1000, message = "Message maksimum 1000 simvol ola biler")
+        @Schema(example = "Order payment completed successfully")
         String message,
+        @Schema(example = "NEW")
         NotificationStatus status
 ) {
 }
--- notification_service/src/main/java/com/abbtech/notification/dto/RequestNotificationStatusDto.java
+++ notification_service/src/main/java/com/abbtech/notification/dto/RequestNotificationStatusDto.java
@@ -1,12 +1,19 @@
 package com.abbtech.notification.dto;
 
 import com.abbtech.notification.model.enums.NotificationStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
+@Schema(example = """
+        {
+          "status": "READ"
+        }
+        """)
 public record RequestNotificationStatusDto(
         @NotNull(message = "Status bos ola bilmez")
+        @Schema(example = "READ")
         NotificationStatus status
 ) {
 }
